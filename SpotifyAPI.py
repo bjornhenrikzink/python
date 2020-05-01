edb67b989abd5f3af90823e4a17f47ff378cb909
Zink Bjorn-Henrik (SEIT) <Bjorn-Henrik.Zink@hm.com>
2020-05-01 05:29:42 +0000

    Added a comment

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -4,6 +4,7 @@ import json
 token = "BQBqeF65Osc7fO1XTDpuZ3_EioaxqqI1PMcZSFGFIs6YD3w-uZJSb7wcQD3PzbVlzuTND_dNYqjeNnoZFw0D66k_o0KJi7l8iLasYzESP2-Gphk8dztV4L_bHiCRHt-qd2UryqHWunX_w_AA0GQT746jadq2JW8VeoJvKKQvaP8_0-pdaKVui9lBAJhHmUuIlGmZCJxhxoZXxPmazj9xcvofFmhQivbrT3bwVEkUBnWl3NYKev_hEoeJ6-wGDUzCdECqr_AUvkxDpEKWu8klzx4" 
 authorization = "'Bearer " + token + "'" 
 
+#print('')
 #print(authorization) 
 #print('') 
 
